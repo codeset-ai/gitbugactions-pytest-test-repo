fd90a022e97988819f93abdc8828dd75e5f50776
André Silva <andre15andre@hotmail.com>
2023-06-09 21:18:27 +0200

    initial implementation

---
+++ app/app.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    @staticmethod
+    def sumInt(a: int, b: int) -> int:
+        return a + b
+
+    @staticmethod
+    def sumFloat(a: float, b: float) -> float:
+        return a + b
+
+    @staticmethod
+    def sum(a, b):
+        return a - b
---
+++ test/test_app.py
@@ -0,0 +1,25 @@
+from app.app import Calculator
+
+import pytest
+
+"""
+Test the sumInt function
+"""
+@pytest.mark.parametrize("a, b, c", [
+    (1, 1, 2),
+    (1, 2, 3),
+    (1, -1, 0)
+])
+def test_sumInt(a, b, c):
+    assert Calculator.sumInt(a, b) == c
+    
+"""
+Test the sumFloat function
+"""
+@pytest.mark.parametrize("a, b, c", [
+    (1.0, 1.0, 2.0),
+    (1.0, 2.0, 3.0),
+    (1.0, -1.0, 0.0)
+])
+def test_sumFloat(a, b, c):
+    assert Calculator.sumFloat(a, b) == c
